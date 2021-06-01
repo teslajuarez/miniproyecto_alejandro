57be095cf1e1dadf2653598fdeeeda0eb7c9d0f7
teslajuarez <83963091+teslajuarez@users.noreply.github.com>
2021-06-01 19:03:10 -0400

    Update Database.php

--- codeingniter/app/Config/Database.php
+++ codeingniter/app/Config/Database.php
@@ -33,8 +33,8 @@ class Database extends Config
 	public $default = [
 		'DSN'      => '',
 		'hostname' => 'localhost',
-		'username' => '',
-		'password' => '',
+		'username' => 'goldem64',
+		'password' => '123456AZ',
 		'database' => '',
 		'DBDriver' => 'MySQLi',
 		'DBPrefix' => '',
