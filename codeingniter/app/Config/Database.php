4da857dd8d76c406d2b6837560b747adf77b165b
teslajuarez <83963091+teslajuarez@users.noreply.github.com>
2021-06-10 10:19:42 -0400

    Update Database.php

--- codeingniter/app/Config/Database.php
+++ codeingniter/app/Config/Database.php
@@ -35,7 +35,7 @@ class Database extends Config
 		'hostname' => 'localhost',
 		'username' => 'goldem64',
 		'password' => '123456AZ',
-		'database' => '',
+		'database' => 'miniproyecto',
 		'DBDriver' => 'MySQLi',
 		'DBPrefix' => '',
 		'pConnect' => false,
