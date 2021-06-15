d076efda2ec20f2ed49d36721f4ca114611a49b0
teslajuarez <83963091+teslajuarez@users.noreply.github.com>
2021-06-15 10:48:21 -0400

    Update listado.php

--- codeingniter/app/Views/listado.php
+++ codeingniter/app/Views/listado.php
@@ -44,7 +44,7 @@
                                 <th><?php echo $dato->apellido_paterno ?></th>
                                 <th><?php echo $dato->apellido_materno ?></th>
                                 <th> <a href="<?php base_url('/index.php/editar') ?>" class="btn btn-sm btn-warning">Editar</a></th>
-                                <th> <a href="<?php base_url('/index.php/eliminar') ?>" class="btn btn-sm btn-darger">Editar</a></th>
+                                <th> <a href="<?php base_url('/index.php/eliminar') ?>" class="btn btn-sm btn-darger">Eliminar</a></th>
                               </tr>
                              <?php endforeach ?>
                              
