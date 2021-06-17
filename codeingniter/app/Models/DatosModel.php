b66c9fb4824df7b743e62caaa7408fdced129899
teslajuarez <83963091+teslajuarez@users.noreply.github.com>
2021-06-17 11:41:48 -0400

    Update DatosModel.php

--- codeingniter/app/Models/DatosModel.php
+++ codeingniter/app/Models/DatosModel.php
@@ -3,9 +3,8 @@ namespace App\Models;
   use CodeIgniter\Model;
 
 class DatosModel extends Model { 
-   public function listar(){
-  $m = $this ->db ->query("SELECT * from persona");
-         return $m->getResult();
-  }
+   public function obtenerInformacion($data){
+   $gModel
 
 }
+  />
