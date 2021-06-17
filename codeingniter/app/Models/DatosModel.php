dc4077130acbadabf8d330679833c9c7a7dc781d
teslajuarez <83963091+teslajuarez@users.noreply.github.com>
2021-06-17 12:25:00 -0400

    Update DatosModel.php

--- codeingniter/app/Models/DatosModel.php
+++ codeingniter/app/Models/DatosModel.php
@@ -4,7 +4,41 @@ namespace App\Models;
 
 class DatosModel extends Model { 
    public function obtenerInformacion($data){
-   $gModel
-
+   $gModel = $this ->db->table('persona');
+   $gModel->where($data);
+   return $gModel->get()->getResultArray();
+     
 }
-  />
+  
+   public function listarTodo(){
+     $gModel = $this ->db->query("SELECT = FROM persona");
+     return $gModel-> getresult();
+}
+  
+  public function insertar($data){
+    $gModel = $this ->db->table('persona');
+    $gModel->insert($data);
+    return $this->db->insertID();
+    
+  }
+  public function actualizar($data,$id){
+  $gModel = $this ->db->table('persona');
+  $gModel->set($data); 
+  $gModel->where($id); 
+  return  $gModel->update();
+    
+  }
+  
+  
+  
+  public function eliminar($id){
+   $gModel = $this ->db->table('persona'); 
+   $gModel->where($id);
+   return  $gModel->delete();
+   
+  }
+  
+}
+  
+  
+
