f89aa31a9dda8ae81652fdb285f9024ac9c5c6ea
teslajuarez <83963091+teslajuarez@users.noreply.github.com>
2021-06-18 12:25:31 -0400

    Update DatosModel.php

--- codeingniter/app/Models/DatosModel.php
+++ codeingniter/app/Models/DatosModel.php
@@ -11,7 +11,7 @@ class DatosModel extends Model {
 }
   
    public function listarTodo(){
-     $gModel = $this->db->query("SELECT = FROM persona");
+     $gModel = $this->db->query("SELECT * FROM persona");
      return $gModel-> getResult();
 }
   
